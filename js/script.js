ae02171402b8248b2654fc016d64e2f685996075
RicTieri <tieri.riccardo@gmail.com>
2023-12-12 13:27:46 +0100

    added condition to print only when loaded

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ createApp({
   },
   methods: {
    getRandomEmail(num){
-    for (let i = 0; i <= num ; i++) {
+    for (let i = 1; i <= num ; i++) {
       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
       .then(response => this.emailList.push(response.data.response))
     }
