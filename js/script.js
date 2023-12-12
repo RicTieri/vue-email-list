1377783d2bb9d47209ba2991215fa457d38a15e7
RicTieri <tieri.riccardo@gmail.com>
2023-12-12 13:25:37 +0100

    added print of ten random email via api

--- js/script.js
+++ js/script.js
@@ -3,10 +3,18 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      
+      emailList: []
     }
   },
   methods: {
-   
+   getRandomEmail(num){
+    for (let i = 0; i <= num ; i++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then(response => this.emailList.push(response.data.response))
+    }
+   },
+  },
+  created(){
+    this.getRandomEmail(10)
   }
 }).mount('#app')
